f82517a40a6aea878cc93f01eae1ae95f181e3be
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2022-04-20 17:42:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import kea.kea as kea
-from kstream.decoupeur import Decoupeur
+from kstream.decoupeur import Decoupeur, StatusDisplay
 import os
+
+ks_debug = False
+ks_discret = True
+
+cp = StatusDisplay(ks_discret)
 
 kea.start("D off")
 
@@ -17,9 +22,9 @@ while True:
     while not code or code[-1] != "":
         code.append(input("KS $ "))
 
-    sortie = Decoupeur("\n".join(code), 0, 1).start()
+    sortie = Decoupeur("\n".join(code), ks_debug, ks_discret).start()
 
     if sortie != 0:
         parsed = "\n".join([" ".join(k) for k in sortie])
-        
+
         kea.start(parsed, 0)
